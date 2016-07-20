bb3432eac9f4d2bcf4d54c1c189c95b8dd429863
SupportExTraGoZ <stranged96@gmail.com>
2016-07-20 19:20:22 +0300

    Simple Script
    Simple Script

---
+++ RequestSimpleScript/Program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+namespace Simple
+{
+    class Program
+    {
+        private static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
+        private static Orbwalking.Orbwalker Orbwalker;
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Drawing.OnDraw += OnDraw;
+        }
+
+        private static void OnDraw(EventArgs args)
+        {
+                Render.Circle.DrawCircle(Player.Position, Player.AttackRange, System.Drawing.Color.AliceBlue);
+        }
+    }
+}
