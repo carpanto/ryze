d290b2cb68fcb27db3f16d245ebb020c21bc7427
Nathan 'jQuery' L# <nathan@zavox.nl>
2016-07-20 18:48:37 +0200

    hehe xd

--- RequestSimpleScript/Program.cs
+++ RequestSimpleScript/Program.cs
@@ -1,22 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LeagueSharp;
-using LeagueSharp.Common;
-using SharpDX;
-using Color = System.Drawing.Color;
-
 namespace Simple
 {
+    using LeagueSharp;
+    using LeagueSharp.Common;
+
     class Program
     {
-        private static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
-        private static Orbwalking.Orbwalker Orbwalker;
         private static void Main(string[] args)
         {
-            CustomEvents.Game.OnGameLoad += eventArgs => Drawing.OnDraw += args1 => Render.Circle.DrawCircle(Player.Position, Player.AttackRange, Color.AliceBlue);
+            CustomEvents.Game.OnGameLoad += eventArgs => Drawing.OnDraw += args1 => Render.Circle.DrawCircle(ObjectManager.Player.Position, ObjectManager.Player.AttackRange, System.Drawing.Color.AliceBlue);
         }
     }
 }
