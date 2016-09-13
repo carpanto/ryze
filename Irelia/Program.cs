ee78bee7e64cb9dbc69b0ceca8847dad3e82f5b9
SupportExTraGoZ <stranged96@gmail.com>
2016-09-13 21:15:16 +0300

    Big Irelia Update :package:
    Big Irelia Update :package:

--- Irelia/Program.cs
+++ Irelia/Program.cs
@@ -14,7 +14,7 @@ using LeagueSharp.Common;
 using LeagueSharp.Data;
 using SharpDX;
 using SebbyLib;
-using Orbwalking = SebbyLib.Orbwalking;
+using ItemData = LeagueSharp.Common.Data.ItemData;
 
 namespace SVIrelia
 {
@@ -26,6 +26,12 @@ namespace SVIrelia
         public static Spell Q, W, E, R;
         public static Menu Menu;
         public static SebbyLib.Orbwalking.Orbwalker Orbwalker;
+
+        public static Items.Item
+            Sheen = new Items.Item(2057),
+            Trinity = new Items.Item(3078),
+            Gauntlet = new Items.Item(3025),
+            LichBane = new Items.Item(3100);
 
         /// <summary>
         /// ObjectManager => Player
@@ -74,7 +80,7 @@ namespace SVIrelia
         private static void WUsage(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
             if (sender.IsMe && args.SData.IsAutoAttack() && args.Target is Obj_AI_Hero &&
-                Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && W.IsReady())
+                Orbwalker.ActiveMode == SebbyLib.Orbwalking.OrbwalkingMode.Combo && W.IsReady())
             {
                 W.Cast();
             }
@@ -95,7 +101,7 @@ namespace SVIrelia
                     combomenu.AddItem(new MenuItem("w.combo", "Use (W)")).SetValue(true);
                     combomenu.AddItem(new MenuItem("e.combo", "Use (E)")).SetValue(true);
                     combomenu.AddItem(new MenuItem("r.combo", "Use (R)")).SetValue(true);
-                    combomenu.AddItem(
+                    /*combomenu.AddItem(
                         new MenuItem("rinfo", ":: R Info").SetTooltip(
                                 "It'll cast R's at enemy once you've activated the ult once.")
                             .SetFontStyle(FontStyle.Bold, SharpDX.Color.DeepPink));
@@ -106,22 +112,33 @@ namespace SVIrelia
                      };
                     combomenu.AddItem(new MenuItem("burst.enemy.killable.check", "Only Enemy Killable")
                         .SetValue(true).SetFontStyle(System.Drawing.FontStyle.Bold))
-                        .Show(combomenu.Item("combo.style").GetValue<StringList>().SelectedIndex == 0);
+                        .Show(combomenu.Item("combo.style").GetValue<StringList>().SelectedIndex == 0);*/
                     Menu.AddSubMenu(combomenu);
                 }
                 var laneclearmenu = new Menu(":: LaneClear Settings", "laneclearsettings");
                 {
-                    laneclearmenu.AddItem(new MenuItem("LCQ", "Use (Q)")).SetValue(true);
-                    laneclearmenu.AddItem(new MenuItem("LCW", "Use (W)")).SetValue(true);
-                    laneclearmenu.AddItem(new MenuItem("LCWSlider", "Minimum Minions in Range to (W)"))
-                        .SetValue(new Slider(2, 0, 10));
-                    laneclearmenu.AddItem(new MenuItem("LCE", "Use (E)")).SetValue(false);
+                    laneclearmenu.AddItem(new MenuItem("LCQ", "Use (Q)").SetValue(true));
+                    laneclearmenu.AddItem(new MenuItem("LCW", "Use (W)").SetValue(true));
+                    laneclearmenu.AddItem(new MenuItem("LCWSlider", "Minimum Minions in Range to (W)")
+                        .SetValue(new Slider(2, 0, 10)));
+                    laneclearmenu.AddItem(new MenuItem("LCE", "Use (E)").SetValue(false));
+                    laneclearmenu.AddItem(
+                        new MenuItem("LaneClearManaManager", "LaneClear Mana Manager (%)").SetValue(new Slider(30, 0, 100)));
                     Menu.AddSubMenu(laneclearmenu);
+                }
+                var jungleclearmenu = new Menu(":: Jungle Clear Settings", "jungleclearsettings");
+                {
+                    jungleclearmenu.AddItem(new MenuItem("JGCQ", "Use (Q)").SetValue(true));
+                    jungleclearmenu.AddItem(new MenuItem("JGCW", "Use (W)").SetValue(true));
+                    jungleclearmenu.AddItem(new MenuItem("JGCE", "Use (E)").SetValue(false));
+                    jungleclearmenu.AddItem(
+                        new MenuItem("JungleClearManaManager", "JungleClear Mana Manager (%)").SetValue(new Slider(30, 0, 100)));
+                    Menu.AddSubMenu(jungleclearmenu);
                 }
                 var harassmenu = new Menu(":: Harass Settings", "harasssettings");
                 {
-                    harassmenu.AddItem(new MenuItem("HarassQ", "Use (Q)")).SetValue(true);
-                    harassmenu.AddItem(new MenuItem("HarassE", "Use (E)")).SetValue(true);
+                    harassmenu.AddItem(new MenuItem("HarassQ", "Use (Q)").SetValue(true));
+                    harassmenu.AddItem(new MenuItem("HarassE", "Use (E)").SetValue(true));
                     harassmenu.AddItem(
                         new MenuItem("HarassManaManager", "Harass Mana Manager (%)").SetValue(new Slider(30, 0, 100)));
                     Menu.AddSubMenu(harassmenu);
@@ -131,11 +148,13 @@ namespace SVIrelia
                     drawingmenu.AddItem(new MenuItem("DrawAA", "Draw (AA) Range").SetValue(false));
                     drawingmenu.AddItem(new MenuItem("DrawQ", "Draw (Q) Range").SetValue(true));
                     drawingmenu.AddItem(new MenuItem("DrawR", "Draw (R) Range").SetValue(true));
+                    drawingmenu.AddItem(new MenuItem("DrawStunnable", "Draw Stunnable?").SetValue(true));
                     Menu.AddSubMenu(drawingmenu);
                 }
                 var miscmenu = new Menu(":: Misc Menu", "miscmenu");
                 {
                     miscmenu.AddItem(new MenuItem("HitChance", "Hit Chance").SetValue(new StringList(new[] { "Medium", "High", "Very High" }, 1)));
+                    miscmenu.AddItem(new MenuItem("KSCheck", "KS if Possible?").SetValue(true));
                     Menu.AddSubMenu(miscmenu);
                 }
 
@@ -181,7 +200,7 @@ namespace SVIrelia
 
             if (Q.IsReady() && Player.Mana > Q.Instance.ManaCost)
             {
-                damage += Q.GetDamage(enemy);
+                damage += Q.GetDamage(enemy) + GetSheenDamage(enemy);
             }
             if (W.IsReady() && Player.Mana > W.Instance.ManaCost)
             {
@@ -212,16 +231,18 @@ namespace SVIrelia
 
             switch (Orbwalker.ActiveMode)
             {
-                    case Orbwalking.OrbwalkingMode.Combo:
+                    case SebbyLib.Orbwalking.OrbwalkingMode.Combo:
                     Combo();
                     break;
-                    case Orbwalking.OrbwalkingMode.LaneClear:
+                    case SebbyLib.Orbwalking.OrbwalkingMode.LaneClear:
+                    //JungleClear();
                     LaneClear();
                     break;
-                    case Orbwalking.OrbwalkingMode.Mixed:
+                    case SebbyLib.Orbwalking.OrbwalkingMode.Mixed:
                     Harass();
                     break;
             }
+            KSChecking();
         }
 
         /// <summary>
@@ -236,6 +257,17 @@ namespace SVIrelia
                 Render.Circle.DrawCircle(ObjectManager.Player.Position, Q.Range, System.Drawing.Color.Chartreuse);
             if (Menu.Item("DrawR").GetValue<bool>() && R.Level > 0 && R.Instance.IsReady())
                 Render.Circle.DrawCircle(ObjectManager.Player.Position, R.Range, System.Drawing.Color.DeepPink);
+
+            if (!Menu.Item("DrawStunnable").GetValue<bool>())
+                return;
+
+            foreach (var enemy in
+                ObjectManager.Get<Obj_AI_Hero>().Where(x => Stunnable(x) && x.IsValidTarget()))
+            {
+                var drawPos = Drawing.WorldToScreen(enemy.Position);
+                var textSize = Drawing.GetTextExtent("Stunnable");
+                Drawing.DrawText(drawPos.X - textSize.Width / 2f, drawPos.Y, System.Drawing.Color.DeepPink, "Stunnable");
+            }
         }
 
         /// <summary>
@@ -243,6 +275,77 @@ namespace SVIrelia
         /// </summary>
         /// <returns></returns>
         public static bool HasRBuff() => Player.HasBuff("ireliatranscendentbladesspell");
+
+        /// <summary>
+        /// Stunnable?
+        /// </summary>
+        /// <param name="unit"></param>
+        /// <returns></returns>
+        public static bool Stunnable(Obj_AI_Hero enemy)
+        {
+            return enemy.HealthPercent > Player.HealthPercent;
+        }
+
+        /// <summary>
+        /// Custom Made GetSheenDamage function -> Including all Sheen buildable items.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static float GetSheenDamage(Obj_AI_Base target)
+        {
+            float totalDamage = 0;
+
+            if (Items.HasItem(3057, Player))
+            {
+                if (Items.CanUseItem(3057))
+                {
+                    totalDamage +=
+                        (float)Player.CalcDamage(target, Damage.DamageType.Physical, Player.TotalAttackDamage);
+                }
+            }
+            else if (Items.HasItem(3025, Player))
+            {
+                if (Items.CanUseItem(3025))
+                {
+                    totalDamage +=
+                        (float)Player.CalcDamage(target, Damage.DamageType.Physical, Player.TotalAttackDamage);
+                }
+            }
+            else if (Items.HasItem(3078, Player))
+            {
+                if (Items.CanUseItem(3078))
+                {
+                    totalDamage +=
+                        (float)Player.CalcDamage(target, Damage.DamageType.Physical, Player.TotalAttackDamage * 2);
+                }
+            }
+            else if (Items.HasItem(3100, Player))
+            {
+                if (LichBane.IsReady())
+                {
+                    totalDamage +=
+                        (float)Player.CalcDamage(target, Damage.DamageType.Magical, (75 / Player.TotalAttackDamage * 100) + (50 / Player.TotalMagicalDamage * 100));
+                }
+            }
+            return totalDamage;
+        }
+
+        private static void KSChecking()
+        {
+            if (Menu.Item("KSCheck").GetValue<bool>())
+            {
+                var target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Physical);
+
+                if (target == null || !target.IsValidTarget())
+                    return;
+
+                if (target.Health < Q.GetDamage(target) + GetSheenDamage(target) && Q.IsReady())
+                    Q.CastOnUnit(target);
+
+                if (target.Health < E.GetDamage(target) && E.IsReady())
+                    E.CastOnUnit(target);
+            }
+        }
 
         /// <summary>
         /// Combo Mode
@@ -258,16 +361,17 @@ namespace SVIrelia
             if (target == null || !target.IsValidTarget())
                 return;
 
-            var distancebetweenmeandtarget = ObjectManager.Player.ServerPosition.Distance(target.ServerPosition);
-            if (Q.IsReady() && target.Distance(Player.Position) > 650)
+            //Game.PrintChat("Q Damage: " + Q.GetDamage(target) + " Sheen Damage:" + GetSheenDamage(target));
+            //var distancebetweenmeandtarget = ObjectManager.Player.ServerPosition.Distance(target.ServerPosition);
+            /*if (Q.IsReady() && target.Distance(Player.Position) > 650)
             {
                 var gapclosingMinion = ObjectManager.Get<Obj_AI_Minion>().Where(m => m.ServerPosition.Distance(ObjectManager.Player.ServerPosition) < 650 &&
-                    m.IsEnemy && m.ServerPosition.Distance(target.ServerPosition) < distancebetweenmeandtarget && m.Health > 1 && m.Health < Q.GetDamage(m)).OrderBy(m => m.Position.Distance(target.ServerPosition)).FirstOrDefault();
+                    m.IsEnemy && m.ServerPosition.Distance(target.ServerPosition) < distancebetweenmeandtarget && m.Health > 1 && m.Health < Q.GetDamage(m) + GetSheenDamage(m)).OrderBy(m => m.Position.Distance(target.ServerPosition)).FirstOrDefault();
                 if (gapclosingMinion != null)
                 {
                     Q.Cast(gapclosingMinion);
                 }
-            }
+            }*/
 
             if (target.IsValidTarget(Q.Range) && !target.IsValidTarget(Player.AttackRange) && Q.IsReady() && useq)
             {
@@ -309,25 +413,75 @@ namespace SVIrelia
         }
 
         /// <summary>
+        /// Jungle Clear
+        /// </summary>
+        /*private static void JungleClear()
+        {
+            if (Player.ManaPercent < Menu.Item("JungleClearManaManager").GetValue<Slider>().Value)
+                return;
+
+            var jgcq = Menu.Item("JGCQ").GetValue<bool>();
+            var jgcw = Menu.Item("JGCW").GetValue<bool>();
+            var jgce = Menu.Item("JGCE").GetValue<bool>();
+
+            var orbwalkerTarget = Orbwalker.GetTarget();
+            var minion = orbwalkerTarget as Obj_AI_Minion;
+
+            var junglemobs = Cache.GetMinions(Player.Position, Q.Range, MinionTeam.Neutral)
+                        .OrderByDescending(x => x.MaxHealth)
+                        .FirstOrDefault();
+
+            if (junglemobs == null || minion == null)
+                return;
+
+            if (minion.Team == GameObjectTeam.Neutral)
+            {
+                if (jgcq && Q.IsReady())
+                    Q.CastOnUnit(minion);
+
+                if (jgcw && W.IsReady() && junglemobs.Distance(Player.Position) <= 125)
+                    W.Cast();
+
+                if (jgce && E.IsReady() && junglemobs.IsValidTarget(E.Range))
+                    E.CastOnUnit(minion);
+            }
+        }*/
+
+        /// <summary>
         /// LaneClear Mode
         /// </summary>
         public static void LaneClear()
         {
+            if (Player.ManaPercent < Menu.Item("LaneClearManaManager").GetValue<Slider>().Value)
+                return;
             var lcq = Menu.Item("LCQ").GetValue<bool>();
             var lcw = Menu.Item("LCW").GetValue<bool>();
             var lce = Menu.Item("LCE").GetValue<bool>();
             var lcwslider = Menu.Item("LCWSlider").GetValue<Slider>().Value;
 
-            var allMinions =
-                MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.Enemy, MinionOrderTypes.Health);
+            var minionq =
+                MinionManager.GetMinions(Q.Range)
+                    .FirstOrDefault(x => Q.GetDamage(x) + GetSheenDamage(x) > x.Health);
 
-            foreach (var minion in allMinions)
+            var minionwe = MinionManager.GetMinions(275);
+
+            if (minionq == null)
+                return;
+
+            if (lcq && Q.IsReady())
             {
-                if (minion.IsValidTarget() && minion.Health < Q.GetDamage(minion) && Q.IsReady() && lcq)
-                    Q.CastOnUnit(minion);
+                Q.CastOnUnit(minionq);
+            }
 
-                if (allMinions.Count > lcwslider && W.IsReady() && lcw && minion.HealthPercent > 5 && minion.IsValidTarget())
+            if (minionwe == null)
+                return;
+
+            foreach (var minion in minionwe)
+            {
+                if (W.IsReady() && lcw && minion.HealthPercent > 5 && minionwe.Count > lcwslider)
                     W.Cast();
+                if (E.IsReady() && lce && minion.Health < E.GetDamage(minion) && lce)
+                    E.CastOnUnit(minion);
             }
         }
 
