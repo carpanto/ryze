b50ed4af6a0eb279a6aa31969db86b5e5d56abc7
ExTraGoZ <ExTraGoZ@DESKTOP>
2016-11-14 19:30:28 +0200

    SSIvern Small Fixes/Adjustments

--- SSIvern/Program.cs
+++ SSIvern/Program.cs
@@ -18,17 +18,19 @@ namespace SSIvern
         protected static Spell Q, W, E, R;
         protected static Orbwalking.Orbwalker Orbwalker;
         protected static Obj_AI_Base Daisy;
-        protected static bool DaisyStatus = false;
+        protected static bool DaisyStatus;
         protected static Menu Config;
         private static bool SPredictionLoaded;
+
         public static string[] HighChamps =
-            {
-                "Ahri", "Anivia", "Annie", "Ashe", "Azir", "Brand", "Caitlyn", "Cassiopeia", "Corki", "Draven",
-                "Ezreal", "Graves", "Jinx", "Kalista", "Karma", "Karthus", "Katarina", "Kennen", "KogMaw", "Leblanc",
-                "Lucian", "Lux", "Malzahar", "MasterYi", "MissFortune", "Orianna", "Quinn", "Sivir", "Syndra", "Talon",
-                "Teemo", "Tristana", "TwistedFate", "Twitch", "Varus", "Vayne", "Veigar", "VelKoz", "Viktor", "Xerath",
-                "Zed", "Ziggs","Kindred","Jhin"
-            };
+        {
+            "Ahri", "Anivia", "Annie", "Ashe", "Azir", "Brand", "Caitlyn", "Cassiopeia", "Corki", "Draven",
+            "Ezreal", "Graves", "Jinx", "Kalista", "Karma", "Karthus", "Katarina", "Kennen", "KogMaw", "Leblanc",
+            "Lucian", "Lux", "Malzahar", "MasterYi", "MissFortune", "Orianna", "Quinn", "Sivir", "Syndra", "Talon",
+            "Teemo", "Tristana", "TwistedFate", "Twitch", "Varus", "Vayne", "Veigar", "VelKoz", "Viktor", "Xerath",
+            "Zed", "Ziggs", "Kindred", "Jhin"
+        };
+
         //protected static int lvl1, lvl2, lvl3, lvl4;
 
         public SSIvernInit()
@@ -100,6 +102,7 @@ namespace SSIvern
             ComboMenu.AddItem(new MenuItem("ComboUseW", "Use W").SetValue(true));
             ComboMenu.AddItem(new MenuItem("ComboUseE", "Use E").SetValue(true));
             ComboMenu.AddItem(new MenuItem("ComboUseItems", "Use Items?").SetValue(true));
+            ComboMenu.AddItem(new MenuItem("ComboDashIfPossibleQ", "Use Q Again to Dash/Gapclose?").SetValue(false).SetTooltip("Will Use Q in Combo Mode again if, Near enemy, there is no more than 1 enemy => Range (1500)", Color.Chartreuse));
             ComboMenu.AddItem(
                 new MenuItem("SemiManualR", "Semi-Manual R Casting?").SetValue(false)
                     .SetTooltip("True - Script will Auto R | False - You will R when you decide - preferably",
@@ -116,13 +119,14 @@ namespace SSIvern
             DaisyMenu.AddItem(new MenuItem("AutoDaisy", "Auto-Play Daisy?").SetValue(false));
             DaisyMenu.AddItem(new MenuItem("DaisyFooter", ":: Soon More Options! <3"));
 
-            var EWhitelistMenu = Config.AddSubMenu(new Menu(":: (E) Protector", "Protector").SetFontStyle(FontStyle.Bold, Color.HotPink));
+            var EWhitelistMenu =
+                Config.AddSubMenu(new Menu(":: (E) Protector", "Protector").SetFontStyle(FontStyle.Bold, Color.HotPink));
             {
                 //EWhitelistMenu.AddItem(new MenuItem("EProtectionAlly", "Use (E) to Protect?").SetValue(true));
                 foreach (var ally in HeroManager.Allies.Where(x => x.IsValid))
-                {
-                    EWhitelistMenu.AddItem(new MenuItem("EProtectionAlly." + ally.ChampionName, "Protect with (E): " + ally.ChampionName).SetValue(HighChamps.Contains(ally.ChampionName)));
-                }
+                    EWhitelistMenu.AddItem(
+                        new MenuItem("EProtectionAlly." + ally.ChampionName, "Protect with (E): " + ally.ChampionName)
+                            .SetValue(HighChamps.Contains(ally.ChampionName)));
             }
 
             var LaneClearMenu = Config.AddSubMenu(new Menu(":: LaneClear", "LaneClear"));
@@ -220,7 +224,8 @@ namespace SSIvern
             var drawdamage = DrawingMenu.AddSubMenu(new Menu(":: Draw Damage", "drawdamage"));
             {
                 var dmgAfterShave =
-                    drawdamage.AddItem(new MenuItem("SurvivorIvern.DrawComboDamage", "Draw Damage on Enemy's HP Bar").SetValue(true));
+                    drawdamage.AddItem(
+                        new MenuItem("SurvivorIvern.DrawComboDamage", "Draw Damage on Enemy's HP Bar").SetValue(true));
                 var drawFill =
                     drawdamage.AddItem(new MenuItem("SurvivorIvern.DrawColour", "Fill Color", true).SetValue(
                         new Circle(true, System.Drawing.Color.Chartreuse)));
@@ -276,8 +281,8 @@ namespace SSIvern
             {
                 var GapCloser = gapcloser.Sender;
                 if (Config.Item("GapCloserEnemies" + GapCloser.ChampionName).GetValue<bool>() &&
-                    GapCloser.IsValidTarget(400))
-                    Q.Cast(GapCloser.ServerPosition);
+                    GapCloser.IsValidTarget(400) && !Player.HasBuff("ivernqallyjump"))
+                    Q.Cast(gapcloser.End);
                 if (Config.Item("GapCloserAutoE").GetValue<bool>() && GapCloser.IsValidTarget(200))
                     E.CastOnUnit(Player);
             }
@@ -286,7 +291,7 @@ namespace SSIvern
         private void Interrupter2_OnInterruptableTarget(Obj_AI_Hero sender,
             Interrupter2.InterruptableTargetEventArgs args)
         {
-            if (Config.Item("QToInterrupt").GetValue<bool>() && sender.IsValidTarget(Q.Range) && Q.IsReady())
+            if (Config.Item("QToInterrupt").GetValue<bool>() && !Player.HasBuff("ivernqallyjump") && sender.IsValidTarget(Q.Range) && Q.IsReady())
                 Q.Cast(sender);
         }
 
@@ -301,42 +306,39 @@ namespace SSIvern
                            args.SData.MissileSpeed + " | Delay: " + args.SData.DelayCastOffsetPercent);*/
 
             #endregion
+
             if (E.Instance.IsReady())
             {
                 foreach (var hero in HeroManager.Allies)
-                {
                     if (Config.Item("EProtectionAlly." + hero.ChampionName).GetValue<bool>())
-                    {
                         if (hero.Distance(Player.ServerPosition) <= E.Range)
-                        {
                             if (OktwCommon.GetIncomingDamage(hero, 2, true) > 0)
                             {
                                 E.CastOnUnit(hero);
                                 if (Config.Item("NotifyProtectedAlly").GetValue<bool>())
-                                {
                                     Notifications.AddNotification(
-                                        new Notification("Protected " + "Ally (" + hero.ChampionName + ")", 4000).SetTextColor(
-                                            System.Drawing.Color.Chartreuse));
-                                }
+                                        new Notification("Protected " + "Ally (" + hero.ChampionName + ")", 4000)
+                                            .SetTextColor(
+                                                System.Drawing.Color.Chartreuse));
                             }
 
-                        }
-                    }
-                }
                 #region Useless Stuff
-                /*if (AllyTarget != null && AllyTarget is Obj_AI_Hero && sender.Target.HealthPercent <= 50 && SpellDamage + 1000 < sender.Target.Health && AllyTarget.IsValidTarget(E.Range))
-                {
-                    if (hero.IncomeDamage > 0 || hero.MinionDamage > hero.Player.Health)
-                        E.CastOnUnit();
-                    E.CastOnUnit((Obj_AI_Base)AllyTarget);
-                    if (Config.Item("NotifyProtectedAlly").GetValue<bool>())
-                    {
-                        Notifications.AddNotification(
-                            new Notification("Protected " + "(" + AllyTarget.Name + ")", 3000).SetTextColor(
-                                System.Drawing.Color.Chartreuse));
-                    }
-                }*/
+
+/*if (AllyTarget != null && AllyTarget is Obj_AI_Hero && sender.Target.HealthPercent <= 50 && SpellDamage + 1000 < sender.Target.Health && AllyTarget.IsValidTarget(E.Range))
+                                                                {
+                                                                    if (hero.IncomeDamage > 0 || hero.MinionDamage > hero.Player.Health)
+                                                                        E.CastOnUnit();
+                                                                    E.CastOnUnit((Obj_AI_Base)AllyTarget);
+                                                                    if (Config.Item("NotifyProtectedAlly").GetValue<bool>())
+                                                                    {
+                                                                        Notifications.AddNotification(
+                                                                            new Notification("Protected " + "(" + AllyTarget.Name + ")", 3000).SetTextColor(
+                                                                                System.Drawing.Color.Chartreuse));
+                                                                    }
+                                                                }*/
+
                 #region Other Logic
+
                 //Game.PrintChat(sender.Target.Name + " | " + args.Target.Type + " | " + args.SData.Name + " | " + sender.Name + " | " + (Obj_AI_Hero)args.Target);
                 /*if (args.Target.Type == GameObjectType.obj_AI_Hero && Config.Item("EProtectionAlly."+((Obj_AI_Hero)args.Target).ChampionName).GetValue<bool>()
                     &&  > ((Obj_AI_Base)args.Target).Health)
@@ -346,17 +348,20 @@ namespace SSIvern
                         new Notification("Successfully protected " + ((Obj_AI_Hero)args.Target).ChampionName, 3000).SetTextColor(
                             System.Drawing.Color.Chartreuse));
                 }*/
+
                 #endregion
-                /*if (AllyProtect != null)
-                {
-                    E.CastOnUnit(AllyProtect);
-                    if (Config.Item("NotifyProtectedAlly").GetValue<bool>())
-                    {
-                        Notifications.AddNotification(
-                            new Notification("Protected " + "(" + AllyProtect.ChampionName + ")", 3000).SetTextColor(
-                                System.Drawing.Color.Chartreuse));
-                    }
-                }*/
+
+/*if (AllyProtect != null)
+                                                {
+                                                    E.CastOnUnit(AllyProtect);
+                                                    if (Config.Item("NotifyProtectedAlly").GetValue<bool>())
+                                                    {
+                                                        Notifications.AddNotification(
+                                                            new Notification("Protected " + "(" + AllyProtect.ChampionName + ")", 3000).SetTextColor(
+                                                                System.Drawing.Color.Chartreuse));
+                                                    }
+                                                }*/
+
                 #endregion
             }
         }
@@ -447,17 +452,13 @@ namespace SSIvern
         private void Obj_AI_Base_OnCreate(GameObject obj, EventArgs args)
         {
             if (obj.Name == "Ivern_Base_P_cas.troy")
-            {
                 Notifications.AddNotification(
                     new Notification("Jungle Camp claiming.", 4000).SetTextColor(
                         System.Drawing.Color.Chartreuse));
-            }
             if (obj.Name == "Ivern_Base_P_FinishedGround.troy")
-            {
                 Notifications.AddNotification(
                     new Notification("Jungle Camp is now ready.", 4000).SetTextColor(
                         System.Drawing.Color.Chartreuse));
-            }
             if (obj.IsValid && obj.IsAlly && obj is Obj_AI_Minion && (obj.Name.ToLower() == "ivernminion"))
                 Daisy = obj as Obj_AI_Base;
         }
@@ -479,7 +480,6 @@ namespace SSIvern
                 {
                     Render.Circle.DrawCircle(Daisy.Position, 200f, System.Drawing.Color.Chartreuse);
                     if (Config.Item("DrawDaisyStatus").GetValue<bool>())
-                    {
                         switch (DaisyStatus)
                         {
                             case false:
@@ -501,7 +501,6 @@ namespace SSIvern
                             }
                                 break;
                         }
-                    }
                 }
             if (Config.Item("DrawJungleMobPassive").GetValue<bool>())
                 foreach (var junglemob in ObjectManager.Get<Obj_AI_Minion>().Where(x => x.HasBuff("ivernpmanager")))
@@ -580,11 +579,11 @@ namespace SSIvern
             var target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
             if ((target == null) || !target.IsValidTarget())
                 return;
-            if (Player.HasBuff("ivernqallyjump") && (target.CountEnemiesInRange(1000) <= 1))
+            if (Player.HasBuff("ivernqallyjump") && Config.Item("ComboDashIfPossibleQ").GetValue<bool>() && (target.CountEnemiesInRange(1500) <= 1))
                 Q.Cast();
             if (UseE && E.Instance.IsReady() && target.IsValidTarget(E.Range))
                 E.CastOnUnit(Player);
-            if (UseQ && Q.Instance.IsReady())
+            if (UseQ && Q.Instance.IsReady() && !Player.HasBuff("ivernqallyjump"))
                 SebbySpell(Q, target);
             if (!Config.Item("SemiManualR").GetValue<bool>())
                 if (UseR && (Player.CountEnemiesInRange(E.Range) >= ComboMinimumREnemies) && R.Instance.IsReady())
@@ -619,8 +618,8 @@ namespace SSIvern
                         new Notification("Successfully Protected Yourself", 3000).SetTextColor(
                             System.Drawing.Color.Chartreuse));
             }*/
-            if (Config.Item("UseENearbyEnemy").GetValue<bool>() && Player.CountEnemiesInRange(Config.Item("MinimumEnemiesNearEDistance").GetValue<Slider>().Value) >=
-                Config.Item("MinimumEnemiesNearE").GetValue<Slider>().Value && E.IsReady())
+            if (Config.Item("UseENearbyEnemy").GetValue<bool>() && (Player.CountEnemiesInRange(Config.Item("MinimumEnemiesNearEDistance").GetValue<Slider>().Value) >=
+                                                                    Config.Item("MinimumEnemiesNearE").GetValue<Slider>().Value) && E.IsReady())
                 E.CastOnUnit(Player);
         }
 
