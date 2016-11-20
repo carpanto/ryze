e7a4b3a82a0787a3efb7e2b59540fb9835d9dd35
ExTraGoZ <ExTraGoZ@DESKTOP>
2016-11-21 00:16:34 +0200

    JihadismAlgorithm
    JihadismAlgorithm

---
+++ Other Stuff/JihadismAlgorithm/Program.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs.cs" company="JihadAlgorithm">
+//      Copyright (c) JihadAlgorithm. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace JihadAlgorithm
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Clear();
+            var rand = new Random();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Welcome to Jihadism Algorithm");
+            Console.WriteLine("Please enter your name");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            var Name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("======================");
+            var jihadnum = rand.Next(100);
+            Console.WriteLine(Name + " | You're " + jihadnum + "% Jihad");
+            Console.WriteLine(Name + " | You're " + (100 - jihadnum) + "% Christian");
+
+            var key = Console.ReadKey();
+            if (key.Key != ConsoleKey.Escape)
+                Main(null);
+        }
+    }
+}
---
+++ Other Stuff/JihadismAlgorithm/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs.cs" company="JihadAlgorithm">
+//      Copyright (c) JihadAlgorithm. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("JihadAlgorithm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JihadAlgorithm")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("1ed19b8f-6746-426c-ac2a-f084b31d76c8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
