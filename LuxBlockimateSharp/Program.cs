284a5cc0749a8f75412b4f3d6537f141ea09e6ce
ExTraGoZ <ExTraGoZ@DESKTOP>
2016-12-08 02:03:29 +0200

    Revert "Tweak"
    This reverts commit f64eb148cf00b34b55d9ff2c19e9825777a500c8.

--- LuxBlockimateSharp/Program.cs
+++ LuxBlockimateSharp/Program.cs
@@ -1,18 +1,23 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Program.cs" company="LuxBlockimateSharp">
-//      Copyright (c) LuxBlockimateSharp. All rights reserved.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-using System;
-using System.Linq;
+﻿using System;
 using LeagueSharp;
 using LeagueSharp.Common;
+using System.Linq;
 
 namespace LuxBlockimateSharp
 {
-    internal class Program
+    class Program
     {
+
+        #region Declaration
+        private static Spell NocR, KhaR, ShacoQ, GravesW;
+        private static Menu Menu;
+        private static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
+        private static string Nocturne = "Nocturne";
+        private static string KhaZix = "Kha'Zix";
+        private static string Shaco = "Shaco";
+        private static string Graves = "Graves";
+        #endregion
+
         public static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
@@ -21,7 +26,7 @@ namespace LuxBlockimateSharp
         private static void Game_OnGameLoad(EventArgs args)
         {
             //if (Player.ChampionName != Nocturne || Player.ChampionName != KhaZix || Player.ChampionName != Shaco)
-            //return;
+                //return;
 
             var ChampionNoc = Player.ChampionName == Nocturne;
             var ChampionKha = Player.ChampionName == KhaZix;
@@ -29,43 +34,44 @@ namespace LuxBlockimateSharp
             var ChampionGraves = Player.ChampionName == Graves;
 
             #region Spells
-
             if (ChampionNoc)
+            {
                 NocR = new Spell(SpellSlot.R);
+            }
             if (ChampionKha)
+            {
                 KhaR = new Spell(SpellSlot.R);
+            }
             if (ChampionShaco)
+            {
                 ShacoQ = new Spell(SpellSlot.Q);
+            }
             if (ChampionGraves)
+            {
                 GravesW = new Spell(SpellSlot.W);
-
+            }
             #endregion
 
             #region Menu
-
             Menu = new Menu("LuxBlockimate#", "LuxBlockimateSharp", true);
 
-            var Spells = Menu.AddSubMenu(new Menu("Spells", "Spells"));
+            Menu Spells = Menu.AddSubMenu(new Menu("Spells", "Spells"));
             if (ChampionShaco)
-                Spells.AddItem(
-                    new MenuItem("BlockShaco", "Exploit with Q").SetValue(true)
-                        .SetTooltip("Will dodge Lux ultimate with Q (Exploit)"));
+            {
+                Spells.AddItem(new MenuItem("BlockShaco", "Exploit with Q").SetValue(true).SetTooltip("Will dodge Lux ultimate with Q (Exploit)"));
+            }
             if (ChampionNoc)
             {
-                Spells.AddItem(
-                    new MenuItem("BlockNoc", "Exploit with R").SetValue(true)
-                        .SetTooltip("Will dodge Lux ultimate with R (Exploit)"));
+                Spells.AddItem(new MenuItem("BlockNoc", "Exploit with R").SetValue(true).SetTooltip("Will dodge Lux ultimate with R (Exploit)"));
                 Spells.AddItem(new MenuItem("ProtectAllyNoc", "Protect Ally by using Exploit with R").SetValue(true));
             }
             if (ChampionKha)
-                Spells.AddItem(
-                    new MenuItem("BlockKha", "Exploit with R").SetValue(true)
-                        .SetTooltip("Will dodge Lux ultimate with R (Exploit)"));
+            {
+                Spells.AddItem(new MenuItem("BlockKha", "Exploit with R").SetValue(true).SetTooltip("Will dodge Lux ultimate with R (Exploit)"));
+            }
             if (ChampionGraves)
             {
-                Spells.AddItem(
-                    new MenuItem("BlockGraves", "Exploit with W").SetValue(true)
-                        .SetTooltip("Will dodge Lux ultimate with W (Exploit)"));
+                Spells.AddItem(new MenuItem("BlockGraves", "Exploit with W").SetValue(true).SetTooltip("Will dodge Lux ultimate with W (Exploit)"));
                 Spells.AddItem(new MenuItem("ProtectAllyGraves", "Protect Ally by using Exploit with W").SetValue(true));
             }
 
@@ -73,18 +79,13 @@ namespace LuxBlockimateSharp
             // Lux Ult?
 
             Menu.AddToMainMenu();
-
             #endregion
 
             #region Subscriptions
-
             Game.OnUpdate += Game_OnUpdate;
             Obj_AI_Base.OnProcessSpellCast += Obj_AI_Base_OnProcessSpell;
-
             #endregion
-
-            Game.PrintChat(
-                "<font color='#800040'>[Exploit] LuxBlockimateSharp</font> <font color='#ff6600'>Loaded.</font>");
+            Game.PrintChat("<font color='#800040'>[Exploit] LuxBlockimateSharp</font> <font color='#ff6600'>Loaded.</font>");
         }
 
         public static void Obj_AI_Base_OnProcessSpell(Obj_AI_Base enemy, GameObjectProcessSpellCastEventArgs Spell)
@@ -92,33 +93,42 @@ namespace LuxBlockimateSharp
             if (enemy.IsMe)
                 return;
             if (enemy.IsChampion() && enemy.IsEnemy)
-                if ((Spell.SData.Name.ToLower() == "luxmalicecannon") ||
-                    (Spell.SData.Name.ToLower() == "lux malice cannon") ||
-                    (Spell.SData.Name.ToLower() == "luxmalicecannonmis") ||
-                    (Spell.SData.Name.ToLower() == "lux malice cannon mis") ||
-                    (Spell.SData.DisplayName.ToLower() == "finalspark"))
+            {
+                if (Spell.SData.Name.ToLower() == "luxmalicecannon" || Spell.SData.Name.ToLower() == "lux malice cannon" || Spell.SData.Name.ToLower() == "luxmalicecannonmis" || Spell.SData.Name.ToLower() == "lux malice cannon mis" || Spell.SData.DisplayName.ToLower() == "finalspark")
                 {
                     var rectangle = new Geometry.Polygon.Rectangle(Spell.Start, Spell.End, 50);
                     if (rectangle.IsInside(Player))
                     {
                         if (Player.ChampionName == Shaco)
+                        {
                             ShacoQ.Cast(Player.Position);
+                        }
                         if (Player.ChampionName == Nocturne)
+                        {
                             NocR.Cast();
-                        if ((Player.ChampionName == Graves) && enemy.IsValidTarget(950f))
+                        }
+                        if (Player.ChampionName == Graves && enemy.IsValidTarget(950f))
+                        {
                             GravesW.Cast(enemy.Position);
+                        }
                         if (Player.ChampionName == KhaZix)
+                        {
                             KhaR.Cast();
+                        }
                     }
                     foreach (var ally in HeroManager.Allies.Where(ally => rectangle.IsInside(ally)))
                     {
-                        if ((Player.ChampionName == Graves) && Menu.Item("ProtectAllyGraves").GetValue<bool>() &&
-                            enemy.IsValidTarget(950f))
+                        if (Player.ChampionName == Graves && Menu.Item("ProtectAllyGraves").GetValue<bool>() && enemy.IsValidTarget(950f))
+                        {
                             GravesW.Cast(enemy.Position);
-                        if ((Player.ChampionName == Nocturne) && Menu.Item("ProtectAllyNoc").GetValue<bool>())
+                        }
+                        if (Player.ChampionName == Nocturne && Menu.Item("ProtectAllyNoc").GetValue<bool>())
+                        {
                             NocR.Cast();
+                        }
                     }
                 }
+            }
         }
 
         private static void Game_OnUpdate(EventArgs args)
@@ -133,22 +143,5 @@ namespace LuxBlockimateSharp
         {
             // Draw Lux Ult?
         }
-
-        #region Declaration
-
-        private static Spell NocR, KhaR, ShacoQ, GravesW;
-        private static Menu Menu;
-
-        private static Obj_AI_Hero Player
-        {
-            get { return ObjectManager.Player; }
-        }
-
-        private static readonly string Nocturne = "Nocturne";
-        private static readonly string KhaZix = "Kha'Zix";
-        private static readonly string Shaco = "Shaco";
-        private static readonly string Graves = "Graves";
-
-        #endregion
     }
 }
--- LuxBlockimateSharp/Properties/AssemblyInfo.cs
+++ LuxBlockimateSharp/Properties/AssemblyInfo.cs
@@ -1,16 +1,10 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="LuxBlockimateSharp">
-//      Copyright (c) LuxBlockimateSharp. All rights reserved.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
 [assembly: AssemblyTitle("LuxBlockimateSharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -23,11 +17,9 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("6e39e178-286c-49d8-9d52-10ce41ab2e44")]
 
 // Version information for an assembly consists of the following four values:
@@ -40,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
